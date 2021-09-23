e63779ca40e6a5a8ed96718abda1542ec36e7e15
Bill-QuanZhang <20030612zq@gmail.cpm>
2021-09-23 11:43:13 -0700

    create a code

--- Zhang_Bill_p1q1.py
+++ Zhang_Bill_p1q1.py
@@ -2,3 +2,16 @@
 
 # get score
 
+is_raining = True
+is_cloudy = False
+
+#print(is_raining and is_cloudy)
+
+if is_raining and is_cloudy:
+    print("You'll need an umbrella.")
+elif is_raining and not is_cloudy:
+    print("You'll need an umbrella.")
+elif is_cloudy and not is_raining:
+    print("An umbrella is a good idea.")
+else:
+    print("leave your umbrella at home.")
