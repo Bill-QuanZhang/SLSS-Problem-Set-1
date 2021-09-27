908e8cb1c620f2c534c2570a372aefae1251db6c
Bill-QuanZhang <20030612zq@gmail.cpm>
2021-09-27 12:36:46 -0700

    add introduction

--- Zhang_Bill_p1q1.py
+++ Zhang_Bill_p1q1.py
@@ -1,17 +1,27 @@
 # Quiz Creation Activity
 
-# get score
+# create questions
+# get score if answer right
+# give out the final results
 
-is_raining = True
-is_cloudy = False
+# Chat before the quiz
+print("Welcome!")
+start_answer = input("Are you ready for this quiz?(yes or no)")
+if start_answer == "yes":
+    print("Good! Let's start!")
+elif start_answer == "no":
+    print("OK, take a breath and I will wait for a second.")
+else:
+    print("Sorry, I can't understand what you said.")
 
-#print(is_raining and is_cloudy)
+# Questions
+answer_1 = input("What is the answer for 2^10?")
+answer_2 = input("")
+
+# Scores
+score = 0
 
 if is_raining and is_cloudy:
     print("You'll need an umbrella.")
-elif is_raining and not is_cloudy:
-    print("You'll need an umbrella.")
-elif is_cloudy and not is_raining:
-    print("An umbrella is a good idea.")
 else:
     print("leave your umbrella at home.")
