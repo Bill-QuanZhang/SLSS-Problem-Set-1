6b4edda628823d8d606e0c57eb806da44a41e89b
Bill Zhang <20030612zq@gmail.com>
2021-10-04 12:50:19 -0700

    give result

--- Zhang_Bill_p1q1.py
+++ Zhang_Bill_p1q1.py
@@ -32,11 +32,11 @@ print("Let's start!")
 # answer_4 = input("Did the moon shine itself?") --> no
 # answer_5 = input("What is the pH for pure water?") --> 7
 questions = (
-    ["What is the answer for 2^10?", "1024"]
-    ["What is the color of cloud?","white"]
-    ["What is the second prime number?","3"]
-    ["Did the moon shine itself?",["n","no"]]
-    ["What is the pH for pure water?","7"]
+    ["What is the answer for 2^10?", ["1024"]],
+    ["What is the color of cloud?",["white"]],
+    ["What is the second prime number?",["3"]],
+    ["Did the moon shine itself?",["n","no"]],
+    ["What is the pH for pure water?",["7"]]
 )
 
 # Scores
@@ -44,10 +44,22 @@ score = 0
 
 # answer
 for question in questions:
-    input(question[0])
-    if input.lower().strip("?.,!") in question[1]:
+    print(question[0])
+    if input().lower().strip("?.,!") in question[1]:
         score += 1
         print("Nice work!")
     else:
         score += 0
         print("Ah oh.")
+    time.sleep(2)
+
+# give out the answer
+print(f"Your final score is {score}!")
+if score == 5:
+    print("You did a perfect job!!!")
+elif score >= 3:
+    print("Not bad.")
+elif score >= 1:
+    print("Try to be better next time.")
+else:
+    print("You should work harder!")
