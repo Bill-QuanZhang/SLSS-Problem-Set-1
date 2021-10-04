77b4ddce75edf81f4cadd7c3e887e3b6e6cf5b89
Bill Zhang <20030612zq@gmail.com>
2021-10-04 12:12:07 -0700

    add start questions

--- Zhang_Bill_p1q1.py
+++ Zhang_Bill_p1q1.py
@@ -4,24 +4,36 @@
 # get score if answer right
 # give out the final results
 
+# import
+import time
+
 # Chat before the quiz
 print("Welcome!")
-start_answer = input("Are you ready for this quiz?(yes or no)")
-if start_answer == "yes":
-    print("Good! Let's start!")
-elif start_answer == "no":
-    print("OK, take a breath and I will wait for a second.")
-else:
-    print("Sorry, I can't understand what you said.")
+
+# start quiz
+done = False
+while not done:
+    start_answer = input("Are you ready for this quiz?(yes or no)")
+    if start_answer.lower().strip("?.,!") in ["y","yes"]:
+        print("Great!")
+        done = True
+    elif start_answer.lower().strip("?.,!") in ["n","no"]:
+        print("OK, take a breath and I will wait for a second.")
+        time.sleep(5)
+    else:
+        print("Sorry, I can't understand what you said.")
+        time.sleep(1)
+print("Let's start!")
 
 # Questions
-answer_1 = input("What is the answer for 2^10?")
-answer_2 = input("")
+# answer_1 = input("What is the answer for 2^10?")
+# answer_2 = input("What is the color of cloud?")
+# answer_3 = input("")
+# answer_4 = input("")
+# answer_5 = input("")
 
 # Scores
 score = 0
 
-if is_raining and is_cloudy:
-    print("You'll need an umbrella.")
-else:
-    print("leave your umbrella at home.")
+# answer
+
